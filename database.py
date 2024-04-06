9263bbee10093f91b57eeaa9b249b5a77785490f
aliskadmitr <alisadmitr@bk.ru>
2024-04-06 19:03:57 +0300

    fix: update database.py

--- database.py
+++ database.py
@@ -4,15 +4,17 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 engine = create_async_engine(
-    "sqlite + aiosqlite:///tasks.db"
+    "sqlite+aiosqlite:///tasks.db"
 )
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
+
 class Model(DeclarativeBase):
     pass
 
-#Describe table
+
+# Describe table
 class TaskOrm(Model):
     __tablename__ = "tasks"
 
@@ -20,9 +22,11 @@ class TaskOrm(Model):
     name: Mapped[str]
     description: Mapped[Optional[str]]
 
+
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Model.metadata.create_all())
+
 
 async def delete_tables():
     async with engine.begin() as conn:
